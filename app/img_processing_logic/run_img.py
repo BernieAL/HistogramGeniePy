9f58b39a33d86e5217402f6ee3d04201706e52de
BernieAL <balmanzar883@gmail.com>
2021-10-23 01:58:27 -0400

    Image being passed from route handler to dom color function

--- app/img_processing_logic/run_img.py
+++ app/img_processing_logic/run_img.py
@@ -1,25 +1,58 @@
+# https://gist.github.com/skt7/71044f42f9323daec3aa035cd050884e
+# https://www.timpoulsen.com/2018/finding-the-dominant-colors-of-an-image.html
+# https://www.pyimagesearch.com/start-here/
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import cv2
+from sklearn.cluster import KMeans
 
 
-# img = cv2.imread(r"C:\Users\balma\Documents\Programming\Python\Learning-practice\flask\Histogram_Genie_Dominant_Color\app\test.jpg")
-# print(img)
+# img = cv2.imread(r"C:\Users\balma\Documents\Programming\Python\Learning-practice\flask\Histogram_Genie_Dominant_Color\app\static\uploads\colors.jpg")
+# cv2.imshow('image',img)
+# cv2.waitKey()
+# # # print(img)
 
-
+CLUSTERS = None
+IMAGE = None
+COLORS = None
+LABELS = None
 
 def run_img_func(img):
-    img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.imread(img)
+    # print(img)
 
-    #get rgb values from image to 1D array
-    r, g, b = cv2.split(img2)
-    r = r.flatten()
-    g = g.flatten()
-    b = b.flatten()
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
-    #plotting 
-    fig = plt.figure()
-    ax = Axes3D(fig)
-    ax.scatter(r, g, b)
-    plt.show()
+    img = img.reshape((img.shape[0] * img.shape[1],3))
+
+    
+    
+    kmeans = KMeans(n_clusters = 3)
+    kmeans.fit(img)
+    COLORS = kmeans.cluster_centers_
+    
+    LABELS = kmeans.labels_
+    return COLORS.astype(int)
+
+colors = run_img_func(r"C:\Users\balma\Documents\Programming\Python\Learning-practice\flask\Histogram_Genie_Dominant_Color\app\static\uploads\colors.jpg")
+print(colors)
+    # # cv2.imshow('image',img)
+    # # cv2.waitKey() 
+    # # cv2.destroyAllWindows()
+    # img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # #get rgb values from image to 1D array
+    # r, g, b = cv2.split(img2)
+    # r = r.flatten()
+    # g = g.flatten()
+    # b = b.flatten()
+
+    # #plotting 
+    # fig = plt.figure()
+    # ax = Axes3D(fig)
+    # ax.scatter(r, g, b)
+    # plt.show()
+
+# def print_img_test(img):
+#     print
--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 
+
+# https://roytuts.com/upload-and-display-image-using-python-flask/
+# https://roytuts.com/upload-and-display-multiple-images-using-python-and-flask/
+
+
+
 import os
 from flask import Flask, render_template,request
 import urllib.request
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
+
 
 
 
@@ -15,9 +22,11 @@ UPLOAD_FOLDER = 'app/static/uploads'
 app = Flask(__name__)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
@@ -33,31 +42,45 @@ def upload_form():
 
 @app.route('/',methods=['POST'])
 def upload_image():
+    
+
+    """If file attribute not on reqeust"""
     if 'file' not in request.files:
-        flash('NO FILE')
+        flash('NO file part')
         return redirect(request.url)
     
     file = request.files['file']
-    if file.filename =='':
-        flash('No image selected for upliading')
+
+    if file.filename == '':
+        flash('No image for uploading')
         return redirect(request.url)
 
+    """
+    if file is populated and is of allowed type
+    Pass it a filename and it will return a secure version of it. 
+    This filename can then safely be stored on a regular file system and passed to os.path.join()
+    save file to upliad folder
+    render upload.html and pass it the filename
+    """
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-        #print('upload_image filename: ' + filename)
-        flash('Image successfully uploaded and displayed below')
         
-        return render_template('upload.html',filename = filename)
+        
+        #pass path to run_img function
+        run_img.run_img_func(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+        return render_template('upload.html',filename=filename)
+    
     else:
-        flash('Image type not allowed')
+        flash('Image submitted is not allowed file type')
         return redirect(request.url)
+
 
 @app.route('/display/<filename>')
 def display_image(filename):
-    #print('display_image filename: '+ filename)
-    run_img.run_img_func(filename)
-    return redirect(url_for('static',filename='uploads/'+filename),code = 301)
+        return redirect(url_for('static', filename='uploads/'+filename),code=301)
+
+
 
 if __name__ == '__main__':
 		app.run(debug=True)
